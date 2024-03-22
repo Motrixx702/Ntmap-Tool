44468e76e575660419b5160fdafec97101da6162
Motrixx702 <150792247+Motrixx702@users.noreply.github.com>
2024-03-22 20:49:22 +0300

    Add files via upload

---
+++ ntmap-tool.sh
@@ -0,0 +1,20 @@
+###*#################
+echo "Enter the IP:"
+read v
+echo "Please wait a bit..."
+echo "Click Enter to complete the process:"
+read x
+echo "Enter a contact to be shown:"
+read g
+echo "The operation has been completed successfully"
+####################
+echo "100%"
+echo "80/tcp    open  http"
+echo "20/tcp    open  ftp-data"
+echo "33/tcp    open  dsp"
+echo "21/tcp    open  ftp"
+echo "1057/tcp  open  startron"
+echo "24/tcp    open  priv-mail"
+echo "113/tcp   open  ident"
+echo "5901/tcp  open  vnc-1"
+echo "23/tcp    open  telnet"
